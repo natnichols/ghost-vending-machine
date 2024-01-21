2659a0fa238348c324aaf167d14cf655d9b2ab01
NatNichols <natalie.e.nichols@gmail.com>
2024-01-21 14:55:53 -0500

    create commentSchema

--- models/snack.js
+++ models/snack.js
@@ -2,10 +2,18 @@ import { mongoose } from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  content: String,
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
+}, {
+  timestamps: true
+})
+
 const snackSchema = new Schema({
   name: String,
   inStock: Boolean,
-  owner: {type: Schema.Types.ObjectId, ref: 'Profile'}
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  comments: [commentSchema]
 }, {
   timestamps: true
 })
